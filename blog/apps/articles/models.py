83373dff19025c14456a634fffdbfdce9878490a
Artyom Golubev <artgolubev@bk.ru>
2022-11-17 23:41:13 +0300

    feat: install grappelli admin, add models of artidles and comments

--- blog/apps/articles/models.py
+++ blog/apps/articles/models.py
@@ -1,14 +1,33 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # объяснение, что хранить в БД. Модели назывем в ед.ч.
+# после добавления моделей, надо сделать миграции
 
 class Article(models.Model):
     article_title = models.CharField('Название статьи', max_length=255)
     article_text = models.TextField('Текст статьи')
     pub_date = models.DateTimeField('Дата публикации')
 
+    def __str__(self):
+        return self.article_title
+
+    def pub_recently(self):
+        return self.pub_date >= (timezone.now() - datetime.timedelta(days = 7))
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
 class Comment(models.Model):
     # привязываем эту модель к модели статьи, во время удаления статьи, удаляются и комменты
-    article = models.ForeingKey(Article, on_delete = models.CASCADE)
+    article = models.ForeignKey(Article, on_delete = models.CASCADE)
     author_name = models.CharField('Имя автора', max_length=50)
     comment_text = models.CharField('Текст комментария', max_length=1000)
+
+    def __str__(self):
+        return self.author_name
+
+    class Meta:
+        verbose_name = 'Комментрий'
+        verbose_name_plural = 'Комментрии'
--- blog/settings.py
+++ blog/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
@@ -120,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
